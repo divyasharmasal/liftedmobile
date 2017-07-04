d5889a8e3462cc7e2ae102a3464c0d9f9f3a9a70
Koh Wei Jie <koh_wei_jie@sal.org.sg>
2017-07-04 12:32:01 +0800

    Updated build process

--- build_dev.sh
+++ build_dev.sh
@@ -6,7 +6,7 @@ yes | docker-compose -f docker/docker-compose.prod.yml rm
 docker-compose -f docker/docker-compose.dev.yml build 
 docker-compose -f docker/docker-compose.dev.yml up -d
 
-#echo
-#echo "Docker containers are up. Logs from liftedmobile_dev:"
-#echo
+echo
+echo "Logs from the liftedmobile Docker container:"
+echo
 docker logs -f liftedmobile_dev
