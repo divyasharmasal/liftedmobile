de640ed7da8e93c2b189ef32f18bd9ed32a09b53
Koh Wei Jie <koh_wei_jie@sal.org.sg>
2017-07-07 15:13:50 +0800

    push_prod.sh tweak

--- push_prod.sh
+++ push_prod.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+
 if [ -z "${1}" ]
 then
-    echo "Usage: ./push_prod.sh <ssh command>"
+    echo "Usage: ./push_prod.sh \"<ssh command>\""
     echo "Example: ./push_prod.sh \"ssh -i '/path/to/liftedmobile.pem' ubuntu@52.221.77.72\""
 else
     eval "docker save docker_db | bzip2 | pv | ${1} 'bunzip2 | docker load'"
