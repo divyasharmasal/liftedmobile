6cc0ee4a16e3f937f968e2d5d47fd27dfe64a274
Koh Wei Jie <koh_wei_jie@sal.org.sg>
2017-07-07 10:23:38 +0800

    trying to deploy

--- build_dev.sh
+++ build_dev.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 yes | docker-compose -f docker/docker-compose.dev.yml stop 
-yes | docker-compose -f docker/docker-compose.prod.yml stop
 yes | docker-compose -f docker/docker-compose.dev.yml rm
-yes | docker-compose -f docker/docker-compose.prod.yml rm
 docker-compose -f docker/docker-compose.dev.yml build 
 docker-compose -f docker/docker-compose.dev.yml up -d
 
