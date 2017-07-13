0c7b40c99e53af157eb3f1e066d04f5576ba395e
Koh Wei Jie <koh_wei_jie@sal.org.sg>
2017-07-13 16:59:06 +0800

    Added framework data and extraction code

---
+++ src/lm/app/lifted_framework/extract_framework_data.py
@@ -0,0 +1,139 @@
+"""
+Extract data from the ODS spreadsheets which contain
+the LIFTED framework and courses for 2017.
+(see https://liftedsg.files.wordpress.com/2017/01/lifted-learning-planner-2017-with-insert.pdf)
+"""
+
+#!/usr/bin/env python3
+import pyexcel_odsr
+
+LEVELS_FILE = "levels.ods"
+VENUES_FILE = "venues.ods"
+FORMATS_FILE = "formats.ods"
+NEEDS_FILE = "needs.ods"
+COURSES_FILE = "courses.ods"
+VERTICALS_FILE = "verticals.ods"
+
+
+def read_ods_file(filename):
+    """
+    Wraps pyexcel_odsr.get_data()
+    """
+    data = pyexcel_odsr.get_data(filename)
+    return data
+
+
+def parse_levels():
+    return parse_two_column(read_ods_file(LEVELS_FILE))
+
+
+def parse_venues():
+    return parse_two_column(read_ods_file(VENUES_FILE))
+
+
+def parse_formats():
+    return parse_two_column(read_ods_file(FORMATS_FILE))
+
+
+def parse_needs():
+    return parse_item_rows(read_ods_file(NEEDS_FILE))
+
+
+def parse_courses():
+    return parse_item_rows(read_ods_file(COURSES_FILE))
+
+
+def parse_verticals():
+    """
+    The verticals.ods spreadsheet is a tree, not a list.
+    Each of the 3 verticals has 7-8 categories.
+    """
+
+    ods_data = read_ods_file(VERTICALS_FILE)["Verticals"]
+    headers = ods_data[0]
+
+    i = 0
+    result = []
+    current_vertical = {"categories": []}
+    for row in ods_data[1:]:
+        # stop the loop when the row is empty
+        if len(row) == 0:
+            break
+
+        # first row of each section
+        if len(row) == 5:
+            j = 0
+            # handle the vertical and vertical_question_text
+            for cell in row[0:2]:
+                current_vertical[headers[j]] = cell
+                j += 1
+
+            category_data = {}
+            for cell in row[2:]:
+                category_data[headers[j]] = cell
+                j += 1
+
+            current_vertical["categories"].append(category_data.copy())
+            category_data = {}
+            result.append(current_vertical.copy())
+
+            current_vertical = {"categories": []}
+        # subsequent rows
+        elif len(row) == 3:
+            j = 2
+            category_data = {}
+            for cell in row:
+                category_data[headers[j]] = cell
+                j += 1
+            result[-1]["categories"].append(category_data)
+        i += 1
+
+    return result
+
+
+def parse_item_rows(ods_data):
+    """
+    Where each row contains a distinct item, and the headers
+    represent keys.
+
+    [
+        { header1: value1, header2: value2, ...}
+        { header1: value1, header2: value2, ...}
+    ]
+    """
+    sheet = ods_data[list(ods_data.keys())[0]]
+    headers = sheet[0]
+    result = []
+
+    for row in sheet[1:]:
+        if len(row) == 0:
+            break
+
+        data = {}
+        i = 0
+        for cell in row:
+            data[headers[i]] = cell
+            i += 1
+        result.append(data)
+
+    return result
+
+
+def parse_two_column(ods_data):
+    """
+    For simple two-column sheets:
+    {B1: B2, C1: C2, ...}
+    """
+    data = {}
+    sheet = ods_data[list(ods_data.keys())[0]]
+
+    for row in sheet[1:]:
+        if len(row) != 2:
+            break
+        data[row[0]] = row[1]
+
+    return data
+
+
+if __name__ == "__main__":
+    print(parse_verticals())
--- src/lm/app/models.py
+++ src/lm/app/models.py
@@ -11,3 +11,23 @@ class Option(models.Model):
     index = models.IntegerField()
     text = models.TextField()
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+class JobVertical(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.TextField()
+
+    # the copy-text of the question that the user sees
+    question_text = models.TextField()
+
+
+class LCategory(models.Model):
+    # the ID is the category key (1, 2, 3, etc)
+    id = models.IntegerField(primary_key=True) 
+
+    # e.g. Technical Lawyering
+    title = models.TextField()
+
+    # the copy-text of the question that the user sees
+    # e.g. Administration and problem-solving
+    question_text = models.TextField()
