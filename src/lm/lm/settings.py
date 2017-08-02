8124f8d2f798d41ac295b7e7855f39cf8617798c
Koh Wei Jie <koh_wei_jie@sal.org.sg>
2017-08-02 10:20:58 +0800

    Added django minify

--- src/lm/lm/settings.py
+++ src/lm/lm/settings.py
@@ -67,6 +67,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
+    'htmlmin.middleware.MarkRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'lm.urls'
