2abb5debcf3a194988bbd0f6b66d69cda1dc457d
Koh Wei Jie <koh_wei_jie@sal.org.sg>
2017-08-11 11:02:47 +0800

    refactored to create renderLoader()

--- src/frontend/src/components/screens/Screen.js
+++ src/frontend/src/components/screens/Screen.js
@@ -16,7 +16,7 @@ const renderLoader = () => {
     <div class="pure-g">
       <div class="pure-u-1">
         <div class="load1">
-          <div class="loader">Loading courses...</div>;
+          <div class="loader">Loading courses...</div>
         </div>
       </div>
     </div>
