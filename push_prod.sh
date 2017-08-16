bc3c099611dc270a77287db810d7e104ffb0e808
Koh Wei Jie <koh_wei_jie@sal.org.sg>
2017-08-16 11:07:09 +0800

    updated push_prod to use port 2233

--- push_prod.sh
+++ push_prod.sh
@@ -4,7 +4,7 @@
 if [ -z "${1}" ]
 then
     echo "Usage: ./push_prod.sh \"<ssh command>\""
-    echo "Example: ./push_prod.sh \"ssh -i '/path/to/liftedmobile.pem' ubuntu@52.221.77.72\""
+    echo "Example: ./push_prod.sh \"ssh -p 2233 -i '/path/to/liftedmobile.pem' ubuntu@52.221.77.72\""
 else
     eval "docker save docker_db | bzip2 | pv | ${1} 'bunzip2 | docker load'"
     eval "docker save docker_liftedmobile | bzip2 | pv | ${1} 'bunzip2 | docker load'"
